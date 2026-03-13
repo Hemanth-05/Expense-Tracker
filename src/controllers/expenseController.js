5472efe6e16524e3d8f18ea3cdadba6d25e93815
Hemanth-05 <hemanthnath123@gmail.com>
2026-03-13 18:39:11 -0400

    Implemented fetching expenses feature

--- src/controllers/expenseController.js
+++ src/controllers/expenseController.js
@@ -1,9 +1,18 @@
-import { addExpense } from '../services/expenseService.js';
+import { addExpense, getExpenses } from '../services/expenseService.js';
 
 export async function createExpenseController(req, res, next) {
   try {
     const expense = await addExpense(req.validatedExpense);
     res.status(201).json(expense);
+  } catch (error) {
+    next(error);
+  }
+}
+
+export async function getExpensesController(req, res, next) {
+  try {
+    const expenses = await getExpenses();
+    res.status(200).json(expenses);
   } catch (error) {
     next(error);
   }
